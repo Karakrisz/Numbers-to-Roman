c1d9c54a7296b0c710fe301901129bebdf388203
Krisztian <webevolution13@gmail.com>
2020-07-03 10:52:51 +0200

    Függvény megírása
    megfelelő érték kiíratása

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <?php
 
-function numberToRomanConversion($number)
+function numberToRomanConversion($szam)
 {
     // tömb feltöltése
-    $map =  ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+    $szamok =  ['M' => 1000, 'CM' => 900, 'D' => 500, 'CD' => 400, 'C' => 100, 'XC' => 90, 'L' => 50, 'XL' => 40, 'X' => 10, 'IX' => 9, 'V' => 5, 'IV' => 4, 'I' => 1];
+    $ertek = '';
+    if ($szam > 0) {
+        foreach ($szamok as $roman => $int) {
+            if ($szam >= $int) {
+                $ertek .= $roman; // összefűzés
+                break;
+            }
+        }
+    }
+    return $ertek;
 }
+
+
+echo numberToRomanConversion(5);
